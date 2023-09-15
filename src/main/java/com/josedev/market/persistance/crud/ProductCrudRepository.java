0aa0747731490deced4a0e4f45f060227ce7e8a2
Jose Corredor <jose96corrza@gmail.com>
2023-09-14 21:36:40 -0500

    Query methods and repository annotation

--- src/main/java/com/josedev/market/persistance/crud/ProductCrudRepository.java
+++ src/main/java/com/josedev/market/persistance/crud/ProductCrudRepository.java
@@ -3,8 +3,24 @@ package com.josedev.market.persistance.crud;
 import com.josedev.market.persistance.entities.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductCrudRepository  extends CrudRepository<Producto, Integer> {
     /**
      * The extends only requires two parameters, the entity and main id key type
      */
+
+
+    /**
+     * findBy<nombre_parametro>OrderBy<nombre_parametro>Asc(int idCategoria)
+     *
+     * Its very important htat you must respect the name of then to allow Spring makes the "magic", using camel case
+     *
+     * @param idCategoria
+     * @return List of products
+     */
+    List<Producto> findByIdCategoriaOrderByNombreAsc(int idCategoria);
+
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidadStock, boolean estado);
 }
--- src/main/java/com/josedev/market/persistance/repository/ProductoRepository.java
+++ src/main/java/com/josedev/market/persistance/repository/ProductoRepository.java
@@ -2,14 +2,37 @@ package com.josedev.market.persistance.repository;
 
 import com.josedev.market.persistance.crud.ProductCrudRepository;
 import com.josedev.market.persistance.entities.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
+@Repository // Specific annotation, more than @Component
 public class ProductoRepository {
 
     private ProductCrudRepository productoRepository;
 
     public List<Producto> getAllProducts(){
         return (List<Producto>) productoRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria){
+        return productoRepository.findByIdCategoriaOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad) {
+        return productoRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getProduct(int idProducto){
+        return productoRepository.findById(idProducto);
+    }
+
+    public Producto saveProduct(Producto producto){
+        return productoRepository.save(producto);
+    }
+
+    public void deleteProduct(int idProducto){
+        productoRepository.deleteById(idProducto);
     }
 }
